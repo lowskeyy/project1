5efff1cf6c03873dd9aad154eae550bcbc3415f9
lowskeyy <rizkyyanuarirawan@gmail.com>
2022-09-17 21:27:11 -0700

    add js

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
-// var open = prompt('Siapa kamu ?');
+// darkmode
 
-// alert('Welcome!, ' + open);
+const darkmode = document.querySelector('.gelap');
+darkmode.addEventListener('click', function() {
+    const bg = document.querySelector('.container');
+    bg.style.backgroundImage = 'url(image/bgdark.jpg)';
+    bg.style.backgroundRepeat = 'no-repeat';
+    bg.style.backgroundSize = 'cover';
+    bg.style.color = 'orange';   
+    // bg.style.textShadow = '0px 0px 3px white';
+    bg.style.boxShadow = ' 0px 0px 5px 3px white';
+
+    const bgnav = document.querySelector('.wrapper');
+    bgnav.style.backgroundImage = 'url(image/bgdark.jpg)';
+    bgnav.style.boxShadow = ' 0px 0px 5px 3px white';
+
+    const badan = document.querySelector('.badan');
+    badan.style.backgroundImage = 'url(image/badan.jpg)';
+
+    const footer = document.querySelector('.footer');
+    footer.style.backgroundImage = 'url(image/bgdark.jpg)';
+    footer.style.boxShadow = ' 0px 0px 5px 3px white';
+
+});
+
+const lightkmode= document.querySelector('.terang');
+lightkmode.addEventListener('click', function() {
+    const bg = document.querySelector('.container');
+    bg.style.backgroundImage = 'url(image/wrp-bg.jpg)';
+    bg.style.color = 'black';   
+    bg.style.boxShadow = ' 0px 0px 5px 5px lightpink'
+
+    const bgnav = document.querySelector('.wrapper');
+    bgnav.style.backgroundImage = 'url(image/wrp-bg.jpg)';
+    bgnav.style.boxShadow = ' 0px 0px 5px 5px lightpink';
+
+    const badan = document.querySelector('.badan');
+    badan.style.backgroundImage = 'url(image/bg-pastel.jpg)';
+
+    const footer = document.querySelector('.footer');
+    footer.style.backgroundImage = 'url(image/bg-biru.jpg)';
+    footer.style.boxShadow = ' 0px 0px 5px 5px lightpink'
+}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
